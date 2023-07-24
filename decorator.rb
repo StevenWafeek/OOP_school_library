931592c554431c7918ab06b646ec80887ada7354
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-07-24 20:41:08 +0300

    Fix decorator file

--- decorator.rb
+++ decorator.rb
@@ -1,28 +1,23 @@
 # Base Decorator class
 require_relative 'person'
 
-# Base Decorator class
-class Decorator < Person
+class Decorator < Nameable
   def initialize(nameable)
-    super(nameable.age, parent_permission: nameable.instance_variable_get(:@parent_permission), name: nameable.name)
+    super()
     @nameable = nameable
-  end
-
-  def correct_name
-    @nameable.correct_name
   end
 end
 
 # CapitalizeDecorator class
 class CapitalizeDecorator < Decorator
   def correct_name
-    super.capitalize
+    @nameable.correct_name.capitalize
   end
 end
 
 # TrimmerDecorator class
 class TrimmerDecorator < Decorator
   def correct_name
-    super[0..9]
+    @nameable.correct_name[0..9]
   end
 end
