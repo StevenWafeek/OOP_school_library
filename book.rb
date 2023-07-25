261326e5cc014a0cff22710e9e42908277d9acef
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-07-25 12:54:39 +0300

    Update book file

--- book.rb
+++ book.rb
@@ -1,9 +1,16 @@
 class Book
-  attr_accessor :title, :author, :rentals
-
-  def initialize(title, author)
-    @title = title
-    @author = author
-    @rentals = [] # Initialize rentals as an empty array
+    attr_accessor :title, :author, :rentals
+  
+    def initialize(title, author)
+      @title = title
+      @author = author
+      @rentals = [] # Initialize rentals as an empty array
+    end
+  
+    def add_rental(person, date)
+      rental = Rental.new(date, self, person)
+      @rentals << rental
+      person.link_rental(rental)
+    end
   end
-end
+  
