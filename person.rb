464f120aae32c76be9507250cb9d537313826757
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-08-02 18:02:28 +0300

    update rental and person files

--- person.rb
+++ person.rb
@@ -1,27 +1,54 @@
 class Person
   attr_reader :id, :name, :age
+  attr_accessor :rentals
 
-  @assigned_ids = []
+  @@id_counter = 0
 
   def initialize(name, age)
     @id = generate_id
     @name = name
     @age = age
+    @rentals = []
   end
 
-  def self.assigned_ids
-    @assigned_ids ||= [] # Lazy initialization using ||= to prevent nil
+  def to_hash
+    hash = {
+      'class' => self.class.to_s,
+      'id' => id,
+      'name' => name,
+      'age' => age
+    }
+
+    # Include additional attributes for subclasses
+    if self.is_a?(Student)
+      hash['classroom'] = {
+        'label' => classroom.label
+      }
+    elsif self.is_a?(Teacher)
+      hash['specialization'] = specialization
+    end
+
+    hash
+  end
+
+  def self.from_hash(person_data)
+    if person_data['class'] == 'Student'
+      classroom = Classroom.new(person_data['classroom']['label'])
+      Student.new(person_data['name'], person_data['age'], classroom)
+    elsif person_data['class'] == 'Teacher'
+      Teacher.new(person_data['name'], person_data['age'], person_data['specialization'])
+    else
+      raise ArgumentError, 'Invalid person class in JSON data'
+    end
+  end
+
+  def add_rental(rental)
+    @rentals << rental
   end
 
   private
 
   def generate_id
-    new_id = nil
-    loop do
-      new_id = rand(1000..9999)
-      break unless self.class.assigned_ids.include?(new_id)
-    end
-    self.class.assigned_ids << new_id
-    new_id
+    @@id_counter += 1
   end
 end
--- rental.rb
+++ rental.rb
@@ -1,17 +1,37 @@
 class Rental
   attr_accessor :date, :book, :person
 
-  @assigned_ids = []
-
   def initialize(date, book, person)
     @date = date
     @book = book
     @person = person
-    book.rentals << self
-    person.rentals << self
+    register_rental
   end
 
-  class << self
-    attr_reader :assigned_ids
+  def self.from_hash(hash, books, people)
+    book_title = hash['book']['title']
+    book_author = hash['book']['author']
+    book = books.find { |b| b.title == book_title && b.author == book_author }
+
+    person_id = hash['person_id']
+    person = people.find { |p| p.id == person_id }
+
+    self.new(hash['date'], book, person)
+  end
+
+  def to_hash
+    {
+      'date' => @date,
+      'book' => @book.to_hash,
+      'person_id' => @person.id,
+      'classroom_label' => @person.classroom&.label
+    }
+  end
+
+  private
+
+  def register_rental
+    @book.add_rental(self) if @book
+    @person.add_rental(self) if @person
   end
 end
