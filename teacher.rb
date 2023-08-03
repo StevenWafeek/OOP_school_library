f9382cbe3838659b174e2824e58e46776993bfae
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-08-03 20:11:23 +0300

    Fix error in teacher file

--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,8 @@
 require './person'
 
 class Teacher < Person
+  attr_accessor :specialization
+
   def initialize(specialization:, age:, name: 'Unknown', parent_permission: true, id: nil)
     super(id: id, age: age, name: name, parent_permission: parent_permission)
     @specialization = specialization
